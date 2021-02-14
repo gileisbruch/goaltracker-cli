308611afcf62287c2b0b8b2d432ca545e86cdf38
Gil Eisbruch <gil.eisbruch@gmail.com>
2021-02-14 13:55:59 -0500

    fix syntax error

--- goaltracker/tracker.py
+++ goaltracker/tracker.py
@@ -10,7 +10,7 @@ def track():
     today = f"{datetime.datetime.now():%Y-%m-%d}"
 
     print("today is " + today
-    if (recent == None):
+    if recent == None:
         print("this appears to be the first time you've tracked your goals")
     else:
         print("The most recent data we have is from " + recent)
